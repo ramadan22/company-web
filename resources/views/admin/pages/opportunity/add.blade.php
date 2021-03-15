ed988f331fc6aec4eb6cc4555afb00909b3e5bdb
her_username <seagleax700@gmail.com>
2021-03-16 01:20:22 +0700

    fixed any error on opportunity
    	new file:   .env.example
    	modified:   app/Http/Responses/Backend/Opportunity/OpportunityCreateResponse.php
    	modified:   app/Http/Responses/Backend/Opportunity/OpportunityEditResponse.php
    	modified:   app/Http/Responses/Backend/Opportunity/OpportunityUpdateResponse.php
    	modified:   resources/views/admin/pages/opportunity/add.blade.php
    	modified:   resources/views/admin/pages/opportunity/edit.blade.php

--- resources/views/admin/pages/opportunity/add.blade.php
+++ resources/views/admin/pages/opportunity/add.blade.php
@@ -97,13 +97,13 @@
                                 </div>
                                 <div class="col-md-9">
                                     <label>Answer Is</label>
-                                    <input type="text" name="answer[]" value="{{ request()->old('answer')[0] ?? '' }}"
+                                    <input type="text" name="answer_0[]" value="{{ request()->old('answer')[0] ?? '' }}"
                                         required class="form-control" placeholder="answer" autocomplete="off"
                                     />
                                 </div>
                                 <div class="col-md-2">
                                     <label>Point Reward</label>
-                                    <input type="number" name="point[]" value="{{ request()->old('point')[0] ?? '' }}"
+                                    <input type="number" name="point_0[]" value="{{ request()->old('point')[0] ?? '' }}"
                                         required class="form-control" placeholder="reward" autocomplete="off"
                                     />
                                 </div>
@@ -113,12 +113,12 @@
                                     <input type="text" disabled value="B" class="input-label-answer form-control font-weight-bold"/>
                                 </div>
                                 <div class="col-md-9">
-                                    <input type="text" name="answer[]" value="{{ request()->old('answer')[0] ?? '' }}"
+                                    <input type="text" name="answer_0[]" value="{{ request()->old('answer')[0] ?? '' }}"
                                         required class="form-control" placeholder="answer" autocomplete="off"
                                     />
                                 </div>
                                 <div class="col-md-2">
-                                    <input type="number" name="point[]" value="{{ request()->old('point')[0] ?? '' }}"
+                                    <input type="number" name="point_0[]" value="{{ request()->old('point')[0] ?? '' }}"
                                         required class="form-control" placeholder="reward" autocomplete="off"
                                     />
                                 </div>
@@ -128,12 +128,12 @@
                                     <input type="text" disabled value="C" class="input-label-answer form-control font-weight-bold"/>
                                 </div>
                                 <div class="col-md-9">
-                                    <input type="text" name="answer[]" value="{{ request()->old('answer')[0] ?? '' }}"
+                                    <input type="text" name="answer_0[]" value="{{ request()->old('answer')[0] ?? '' }}"
                                         required class="form-control" placeholder="answer" autocomplete="off"
                                     />
                                 </div>
                                 <div class="col-md-2">
-                                    <input type="number" name="point[]" value="{{ request()->old('point')[0] ?? '' }}"
+                                    <input type="number" name="point_0[]" value="{{ request()->old('point')[0] ?? '' }}"
                                         required class="form-control" placeholder="reward" autocomplete="off"
                                     />
                                 </div>
@@ -183,12 +183,12 @@
                     +'</button>'
                 +'</div>'
                 +'<div class="col-md-9">'
-                    +'<input type="text" name="answer[]" value="{{ request()->old('opportunity_point') ?? '' }}"'
+                    +'<input type="text" name="answer_'+ question +'[]" value="{{ request()->old('opportunity_point') ?? '' }}"'
                         +'class="form-control" placeholder="answer" autocomplete="off"'
                     +'/>'
                 +'</div>'
                 +'<div class="col-md-2">'
-                    +'<input type="number" name="point[]" value="{{ request()->old('opportunity_point') ?? '' }}"'
+                    +'<input type="number" name="point_'+ question +'[]" value="{{ request()->old('opportunity_point') ?? '' }}"'
                         +'class="form-control" placeholder="reward" autocomplete="off"'
                     +'/>'
                 +'</div>'
@@ -232,13 +232,13 @@
                         +'</div>'
                         +'<div class="col-md-9">'
                             +'<label>Answer Is</label>'
-                            +'<input type="text" name="answer[]" required'
+                            +'<input type="text" name="answer_'+ total +'[]" required '
                                 +'class="form-control" placeholder="answer" autocomplete="off"'
                             +'/>'
                         +'</div>'
                         +'<div class="col-md-2">'
                             +'<label>Point Reward</label>'
-                            +'<input type="number" name="point[]" required'
+                            +'<input type="number" name="point_'+ total +'[]" required '
                                 +'class="form-control" placeholder="reward" autocomplete="off"'
                             +'/>'
                         +'</div>'
@@ -248,12 +248,12 @@
                             +'<input type="text" disabled value="B" class="input-label-answer form-control font-weight-bold"/>'
                         +'</div>'
                         +'<div class="col-md-9">'
-                            +'<input type="text" name="answer[]" required'
+                            +'<input type="text" name="answer_'+ total +'[]" required '
                                 +'class="form-control" placeholder="answer" autocomplete="off"'
                             +'/>'
                         +'</div>'
                         +'<div class="col-md-2">'
-                            +'<input type="number" name="point[]" required'
+                            +'<input type="number" name="point_'+ total +'[]" required '
                                 +'class="form-control" placeholder="reward" autocomplete="off"'
                             +'/>'
                         +'</div>'
@@ -263,12 +263,12 @@
                             +'<input type="text" disabled value="C" class="input-label-answer form-control font-weight-bold"/>'
                         +'</div>'
                         +'<div class="col-md-9">'
-                            +'<input type="text" name="answer[]" required'
+                            +'<input type="text" name="answer_'+ total +'[]" required '
                                 +'class="form-control" placeholder="answer" autocomplete="off"'
                             +'/>'
                         +'</div>'
                         +'<div class="col-md-2">'
-                            +'<input type="number" name="point[]" required'
+                            +'<input type="number" name="point_'+ total +'[]" required '
                                 +'class="form-control" placeholder="reward" autocomplete="off"'
                             +'/>'
                         +'</div>'
